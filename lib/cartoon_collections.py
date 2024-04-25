c29330b1e25e68256579a8189e4cdc7901a53c0e
Tejas Nampalli <TSNampalli@gmail.com>
2024-04-25 14:29:01 -0400

    finished

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,23 @@
-def roll_call_dwarves():
+def roll_call_dwarves(dwarves):
+    for i, dwarf in enumerate(dwarves, start= 1):
+        print(f"{i}. {dwarf}")
     pass
 
-def summon_captain_planet():
+def summon_captain_planet(planeteer):
+    return [call.capitalize() + '!' for call in planeteer] 
     pass
 
-def long_planeteer_calls():
+def long_planeteer_calls(calls):
+    for word in calls:
+        if len(word) > 4:
+            return True
+    return False
     pass
 
-def find_the_cheese():
+def find_the_cheese(snacks):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for ingredient in snacks:
+        if ingredient in cheeses:
+            return ingredient
+    return None
     pass
